37da80a8c5a597ad89f13b2fa26876300941fae5
Denis <15inchdesign@users.noreply.github.com>
2020-12-17 07:44:01 +0100

    Added slick slider and mobile navigation.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 class Navbar extends React.Component {
     render(){
+        const title = 'i3 React Test';
         return(
-            <nav className="navbar hidde-sm">
-                <ul>
+            <nav role="navigation" className="navbar">
+                <ul className="hidde-sm">
                     <li>
                         <Link to='/'>Home</Link>
                         <Link to='/about'>About</Link>
+                        <Link to='/' className="logo mobile-logo">{title}</Link>
                         <Link to=''>Menu 1</Link>
                         <Link to=''>Menu 2</Link>
                     </li>
                 </ul>
+                 <div className="mobile-nav">
+                    <div className="tabs">
+                        <div className="tab">
+                            <input type="checkbox" id="chck1" />
+                            <label className="tab-label" htmlFor="chck1"><Link to='/'>Home</Link></label>
+                            <div className="tab-content">
+                                <ul>
+                                    <li><a href="/">Sub item</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="tab">
+                            <Link to='/about'>About</Link>
+                        </div>
+                        <div className="tab">
+                            <Link to='/'>Menu 1</Link>
+                        </div>
+                        <div className="tab">
+                            <Link to='/'>Menu 2</Link>
+                        </div>
+                    </div>   
+                </div>
             </nav>
         );
     }
