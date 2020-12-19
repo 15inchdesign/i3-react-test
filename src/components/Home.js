808f8a98d01eddf1740cf0e182c3c422291e0846
Denis <15inchdesign@users.noreply.github.com>
2020-12-19 03:54:10 +0100

    Tweeks on home page added icons, effects, mobile adapt of slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Slider from './Slider.js';
 
 class Home extends React.Component {
     render(){
         return(
             <div className='home'>
-                <h1>Home</h1>
+                 <Slider />
             </div>
         );
     }
